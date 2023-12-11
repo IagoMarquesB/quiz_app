1a9772d688864348ba7d9fef0bda488854b591d9
IagoMarquesB <iagomarquesbarbosa@gmail.com>
2023-12-11 16:54:01 -0300

    fixes

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -4,11 +4,11 @@ import 'dart:math';
 final questions = [
   const QuizQuestion(
     'Qual comando permite associar um apelido a outro comando?',
-    ['alias', 'atalho', 'aparelho', 'assoc'],
+    ['alias', 'shortcut', 'appliance', 'assoc'],
   ),
   const QuizQuestion(
     'Qual comando é usado para levar um comando pausado para o plano de fundo?',
-    ['bg', 'voltar', 'pausar', 'resumir'],
+    ['bg', 'back', 'pause', 'resume'],
   ),
   const QuizQuestion(
     'O que o comando "cal" faz no shell?',
@@ -21,19 +21,19 @@ final questions = [
   ),
   const QuizQuestion(
     'Qual comando cancela a impressão de documentos na fila?',
-    ['cancel', 'pararimpressao', 'matarimpressao', 'pausarimpressao'],
+    ['cancel', 'stopprint', 'killprint', 'pauseprint'],
   ),
   const QuizQuestion(
     'Qual comando é usado para mostrar o conteúdo de um arquivo no terminal?',
-    ['cat', 'ver', 'lerarquivo', 'exibir'],
+    ['cat', 'view', 'readfile', 'display'],
   ),
   const QuizQuestion(
     'Qual comando altera as permissões de um arquivo?',
-    ['chmod', 'permissões', 'alteraperm', 'arquivoseguro'],
+    ['chmod', 'permissions', 'changeperm', 'securefile'],
   ),
   const QuizQuestion(
     'Qual comando é usado para alterar o proprietário de um arquivo?',
-    ['chown', 'alteraproprietario', 'proprietario', 'chmod'],
+    ['chown', 'changeowner', 'owner', 'chmod'],
   ),
   const QuizQuestion(
     'Qual é o objetivo de um StatefulWidget no Flutter?',
@@ -46,15 +46,15 @@ final questions = [
   ),
   const QuizQuestion(
     'Qual comando é usado para compilar C/C++/etc com a infraestrutura LLVM?',
-    ['clang', 'llvmcc', 'gcc', 'compilar'],
+    ['clang', 'llvmcc', 'gcc', 'compile'],
   ),
   const QuizQuestion(
     'Qual comando é usado para limpar a tela do terminal?',
-    ['clear', 'cls', 'refresh', 'limpar'],
+    ['clear', 'cls', 'refresh', 'clean'],
   ),
   const QuizQuestion(
     'Qual comando executa outro comando em segundo plano?',
-    ['comando &', 'executabg', 'segundoplano', 'iniciabg'],
+    ['comando &', 'runbg', 'background', 'startbg'],
   ),
   const QuizQuestion(
     'Qual comando redireciona a saída de um comando para um arquivo (substituição)?',
@@ -80,63 +80,63 @@ final questions = [
   ),
   const QuizQuestion(
     'Qual comando copia arquivos e diretórios?',
-    ['cp', 'copiar', 'mover', 'duplicar'],
+    ['cp', 'copy', 'move', 'duplicate'],
   ),
   const QuizQuestion(
     'Qual comando exibe a data e hora atual?',
-    ['date', 'time', 'datetime', 'agora'],
+    ['date', 'time', 'datetime', 'now'],
   ),
   const QuizQuestion(
     'Qual comando compara dois arquivos linha por linha?',
-    ['diff', 'comparar', 'linhaalinha', 'verificardiferencas'],
+    ['diff', 'compare', 'linebyline', 'diffcheck'],
   ),
   const QuizQuestion(
     'Qual comando mostra espaço livre no sistema de arquivos?',
-    ['df', 'livre', 'espaco', 'sistemaarquivos'],
+    ['df', 'free', 'space', 'filesystem'],
   ),
   const QuizQuestion(
     'Qual comando mostra o espaço ocupado por diretórios?',
-    ['du', 'usado', 'diretorios', 'espacousado'],
+    ['du', 'usadedspace', 'directories', 'diskspace'],
   ),
   const QuizQuestion(
     'Qual comando exibe mensagens de inicialização e do kernel?',
-    ['dmesg', 'mensageminicializacao', 'mensagemsistema', 'kernelmsg'],
+    ['dmesg', 'kernelmsg', 'bootmsg', 'messages'],
   ),
   const QuizQuestion(
     'Qual comando permite instalar pacotes DEB?',
-    ['dpkg', 'instalar', 'debinstalar', 'gerenciadorpacotes'],
+    ['dpkg', 'install', 'debinstall', 'packetmanager'],
   ),
   const QuizQuestion(
     'Qual comando escreve uma mensagem no terminal?',
-    ['echo', 'escrever', 'imprimir', 'mensagem'],
+    ['echo', 'write', 'print', 'message'],
   ),
   const QuizQuestion(
     'Qual comando permite manipular tabelas de partições de disco?',
-    ['fdisk', 'particoes', 'gerenciadisco', 'tabelaparticoes'],
+    ['fdisk', 'partitions', 'diskmanage', 'partitiontable'],
   ),
   const QuizQuestion(
     'Qual comando traz um comando pausado para o primeiro plano?',
-    ['fg', 'primeiroplano', 'resumir', 'trazerfrente'],
+    ['fg', 'foreground', 'resume', 'bringforward'],
   ),
   const QuizQuestion(
     'Qual comando identifica o tipo de um arquivo?',
-    ['file', 'tipofile', 'identificar', 'infoarquivo'],
+    ['file', 'filetype', 'identifify', 'fileinfo'],
   ),
   const QuizQuestion(
     'Qual é o compilador tradicional de C/C++ no GNU Compiler Collection?',
-    ['gcc', 'compiladorgnu', 'clang', 'compilar'],
+    ['gcc', 'gcompiler', 'clang', 'compile'],
   ),
   const QuizQuestion(
     'Qual comando mostra linhas que correspondem a um padrão em um arquivo?',
-    ['grep', 'encontrarpadrao', 'corresponderlinhas', 'buscapadrao'],
+    ['grep', 'findpattern', 'matchlines', 'searchpattern'],
   ),
   const QuizQuestion(
     'Qual comando exibe o início de um arquivo?',
-    ['head', 'inicio', 'arquivoinicio', 'cabecalo'],
+    ['head', 'beginning', 'startfile', 'headfile'],
   ),
   const QuizQuestion(
     'Qual comando encerra um processo?',
-    ['kill', 'encerrar', 'parar', 'finalizarprocesso'],
+    ['kill', 'terminate', 'stop', 'endprocess'],
   ),
 ];
 final _random = Random();
